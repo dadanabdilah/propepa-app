7843ef04b00619e6b415e382eae78c8583c8548e
fikriks <fikriks130@gmail.com>
2023-08-22 21:29:51 +0700

    add: seeders :rocket:

---
+++ app/Database/Seeds/UserTableSeeder.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\Database\Seeder;
+use CodeIgniter\I18n\Time;
+
+use App\Models\UserModel;
+
+use CodeIgniter\Shield\Models\GroupModel;
+use CodeIgniter\Shield\Models\UserIdentityModel;
+
+class UserTableSeeder extends Seeder
+{
+    public function run()
+    {
+        $userModel = new UserModel;
+        $groupModel = new GroupModel;
+        $userIdentityModel = new UserIdentityModel;
+        $now = Time::now('Asia/Jakarta', 'id_ID');
+
+        $admin = [
+            'username' => 'admin123',
+            'active' => 1
+        ];
+
+        $userModel->insert($admin);
+        $adminId = $userModel->getInsertID();
+
+        $groups = [
+            [
+                'user_id' => $adminId,
+                'group' => 'admin',
+                'created_at' => $now
+            ]
+        ];
+
+        $groupModel->insertBatch($groups);
+
+        $userIdentities = [
+            [
+                'user_id' => $adminId,
+                'type' => 'email_password',
+                'name' => 'Admin',
+                'secret' => 'admin@gmail.com',
+                'secret2' => password_hash(base64_encode(hash('sha384', '123456789', true)), PASSWORD_DEFAULT),
+                'created_at' => $now,
+                'updated_at' => $now
+            ]
+        ];
+
+        $userIdentityModel->insertBatch($userIdentities);
+    }
+}
