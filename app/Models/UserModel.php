950cc3df8241e0a0203734e4e94b5914ac1d703a
fikriks <fikriks130@gmail.com>
2023-08-31 17:06:15 +0700

    add: user model :rocket:

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -17,4 +17,27 @@ class UserModel extends UserMod
         'deleted_at',
         'permissions'
     ];
+
+    function getIdentity()
+    {
+        $this->select('auth_identities.*,auth_identities.id as identity_id, auth_groups_users.*, auth_groups_users.id as group_id,users.*, users.created_at AS first_work_date');
+
+        $data = $this->join('auth_identities', 'users.id = auth_identities.user_id')
+            ->join('auth_groups_users', 'users.id = auth_groups_users.user_id')
+            ->findAll();
+
+        return $data;
+    }
+
+    function getIdentityById($id = null)
+    {
+        $this->select('auth_identities.*,auth_identities.id as identity_id, auth_groups_users.*, auth_groups_users.id as group_id,users.*, users.created_at AS first_work_date');
+
+        $data = $this->join('auth_identities', 'users.id = auth_identities.user_id')
+            ->join('auth_groups_users', 'users.id = auth_groups_users.user_id')
+            ->where('auth_identities.user_id', $id)
+            ->first();
+
+        return $data;
+    }
 }
