dbf40bbbee6ac701b318f93cc68285229ab50237
fikriks <fikriks130@gmail.com>
2023-09-10 18:58:35 +0700

    fix: admin layout :rocket:

--- app/Views/layouts/admin.php
+++ app/Views/layouts/admin.php
@@ -41,10 +41,8 @@
             <!-- Sidebar scroll-->
             <div>
                 <div class="brand-logo d-flex align-items-center justify-content-between">
-                    <a href="./index.html" class="text-nowrap logo-img">
-                        <h2><b>PROPEPA</b></h2>
-                        <!-- <img src="<?= base_url('assets/images/logos/logo.png') ?>" class="dark-logo" width="180" alt="" />
-              <img src="<?= base_url('assets/images/logos/light-logo.svg') ?>" class="light-logo"  width="180" alt="" /> -->
+                    <a href="<?= site_url('admin/dashboard') ?>" class="text-nowrap logo-img">
+                        <img src="<?= base_url('assets/images/logos/logo.png') ?>" class="dark-logo" width="180" alt="" />
                     </a>
                     <div class="close-btn d-lg-none d-block sidebartoggler cursor-pointer" id="sidebarCollapse">
                         <i class="ti ti-x fs-8 text-muted"></i>
@@ -53,20 +51,10 @@
                 <!-- Sidebar navigation-->
                 <nav class="sidebar-nav scroll-sidebar" data-simplebar>
                     <ul id="sidebarnav">
-                        <!-- ============================= -->
-                        <!-- Home -->
-                        <!-- ============================= -->
-                        <li class="nav-small-cap">
-                            <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
-                            <span class="hide-menu">Home</span>
-                        </li>
-                        <!-- =================== -->
-                        <!-- Dashboard -->
-                        <!-- =================== -->
-                        <li class="sidebar-item">
-                            <a class="sidebar-link" href="./index.html" aria-expanded="false">
+                        <li class="sidebar-item mt-4">
+                            <a class="sidebar-link" href="<?= site_url('admin/dashboard') ?>" aria-expanded="false">
                                 <span>
-                                    <i class="ti ti-aperture"></i>
+                                    <i class="ti ti-home-2"></i>
                                 </span>
                                 <span class="hide-menu">Dashboard</span>
                             </a>
@@ -76,7 +64,7 @@
                         <li class="sidebar-item">
                             <a class="sidebar-link" href="<?= site_url('admin/study-references') ?>" aria-expanded="false">
                                 <span>
-                                    <i class="ti ti-aperture"></i>
+                                    <i class="ti ti-layout-grid"></i>
                                 </span>
                                 <span class="hide-menu">Referensi Belajar</span>
                             </a>
@@ -85,7 +73,7 @@
                         <li class="sidebar-item">
                             <a class="sidebar-link" href="<?= site_url('admin/study-modules') ?>" aria-expanded="false">
                                 <span>
-                                    <i class="ti ti-aperture"></i>
+                                    <i class="ti ti-file-description"></i>
                                 </span>
                                 <span class="hide-menu">Modul Belajar</span>
                             </a>
@@ -94,7 +82,7 @@
                         <li class="sidebar-item">
                             <a class="sidebar-link" href="<?= site_url('admin/opinions') ?>" aria-expanded="false">
                                 <span>
-                                    <i class="ti ti-aperture"></i>
+                                    <i class="ti ti-message"></i>
                                 </span>
                                 <span class="hide-menu">Berbagi Opini</span>
                             </a>
@@ -103,7 +91,7 @@
                         <li class="sidebar-item">
                             <a class="sidebar-link" href="<?= site_url('admin/study-communities') ?>" aria-expanded="false">
                                 <span>
-                                    <i class="ti ti-aperture"></i>
+                                    <i class="ti ti-mail"></i>
                                 </span>
                                 <span class="hide-menu">Komunitas Belajar</span>
                             </a>
@@ -112,7 +100,7 @@
                         <li class="sidebar-item">
                             <a class="sidebar-link" href="<?= site_url('admin/sharing-practices') ?>" aria-expanded="false">
                                 <span>
-                                    <i class="ti ti-aperture"></i>
+                                    <i class="ti ti-chart-pie-2"></i>
                                 </span>
                                 <span class="hide-menu">Praktik Berbagi</span>
                             </a>
@@ -121,7 +109,7 @@
                         <li class="sidebar-item">
                             <a class="sidebar-link" href="<?= site_url('admin/users') ?>" aria-expanded="false">
                                 <span>
-                                    <i class="ti ti-aperture"></i>
+                                    <i class="ti ti-users"></i>
                                 </span>
                                 <span class="hide-menu">Pengguna</span>
                             </a>
@@ -130,7 +118,7 @@
                         <li class="sidebar-item">
                             <a class="sidebar-link" href="<?= site_url('logout') ?>" aria-expanded="false">
                                 <span>
-                                    <i class="ti ti-aperture"></i>
+                                    <i class="ti ti-door-exit"></i>
                                 </span>
                                 <span class="hide-menu">Logout</span>
                             </a>
@@ -161,7 +149,7 @@
             <!--  Header Start -->
             <header class="app-header">
                 <nav class="navbar navbar-expand-lg navbar-light">
-                    <ul class="navbar-nav">
+                    <ul class="navbar-nav d-none d-lg-none d-md-block">
                         <li class="nav-item">
                             <a class="nav-link sidebartoggler nav-icon-hover ms-n3" id="headerCollapse" href="javascript:void(0)">
                                 <i class="ti ti-menu-2"></i>
@@ -170,7 +158,7 @@
                     </ul>
                     <div class="d-block d-lg-none">
                         <img src="<?= base_url('assets/images/logos/logo.png') ?>" class="dark-logo" width="180" alt="" />
-                        <img src="<?= base_url('assets/images/logos/light-logo.svg') ?>" class="light-logo" width="180" alt="" />
+                        <img src="<?= base_url('assets/images/logos/logo.png') ?>" class="light-logo" width="180" alt="" />
                     </div>
                     <button class="navbar-toggler p-0 border-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="p-2">
@@ -181,62 +169,32 @@
                         <div class="d-flex align-items-center justify-content-between">
                             <ul class="navbar-nav flex-row ms-auto align-items-center justify-content-center">
                                 <li class="nav-item dropdown">
-                                    <a class="nav-link nav-icon-hover" href="javascript:void(0)" id="drop2" data-bs-toggle="dropdown" aria-expanded="false">
-                                        <i class="ti ti-bell-ringing"></i>
-                                        <div class="notification bg-primary rounded-circle"></div>
-                                    </a>
-                                    <div class="dropdown-menu content-dd dropdown-menu-end dropdown-menu-animate-up" aria-labelledby="drop2">
-                                        <div class="d-flex align-items-center justify-content-between py-3 px-7">
-                                            <h5 class="mb-0 fs-5 fw-semibold">Notifications</h5>
-                                            <span class="badge bg-primary rounded-4 px-3 py-1 lh-sm">1 new</span>
-                                        </div>
-                                        <div class="message-body" data-simplebar>
-                                            <a href="javascript:void(0)" class="py-6 px-7 d-flex align-items-center dropdown-item">
-                                                <span class="me-3">
-                                                    <img src="<?= base_url('assets/images/profile/user-1.jpg') ?>" alt="user" class="rounded-circle" width="48" height="48" />
-                                                </span>
-                                                <div class="w-75 d-inline-block v-middle">
-                                                    <h6 class="mb-1 fw-semibold">John Doe Joined the Team!</h6>
-                                                    <span class="d-block">Congratulate him</span>
-                                                </div>
-                                            </a>
-                                        </div>
-                                        <div class="py-6 px-7 mb-1">
-                                            <button class="btn btn-outline-primary w-100"> See All Notifications </button>
-                                        </div>
-                                    </div>
-                                </li>
-                                <li class="nav-item dropdown">
                                     <a class="nav-link pe-0" href="javascript:void(0)" id="drop1" data-bs-toggle="dropdown" aria-expanded="false">
                                         <div class="d-flex align-items-center">
                                             <div class="user-profile-img">
-                                                <img src="<?= base_url('assets/images/profile/user-1.jpg') ?>" class="rounded-circle" width="35" height="35" alt="" />
+                                                <img src="https://ui-avatars.com/api/?name=<?= auth()->user()->identities[0]->name ?>" class="rounded-circle" width="35" height="35" alt="" />
                                             </div>
                                         </div>
                                     </a>
                                     <div class="dropdown-menu content-dd dropdown-menu-end dropdown-menu-animate-up" aria-labelledby="drop1">
                                         <div class="profile-dropdown position-relative" data-simplebar>
                                             <div class="py-3 px-7 pb-0">
-                                                <h5 class="mb-0 fs-5 fw-semibold">User Profile</h5>
+                                                <h5 class="mb-0 fs-5 fw-semibold">Profil</h5>
                                             </div>
                                             <div class="d-flex align-items-center py-9 mx-7 border-bottom">
-                                                <img src="<?= base_url('assets/images/profile/user-1.jpg') ?>" class="rounded-circle" width="80" height="80" alt="" />
+                                                <img src="https://ui-avatars.com/api/?name=<?= auth()->user()->identities[0]->name ?>" class="rounded-circle" width="80" height="80" alt="" />
                                                 <div class="ms-3">
-                                                    <h5 class="mb-1 fs-3">Mathew Anderson</h5>
-                                                    <span class="mb-1 d-block text-dark">Designer</span>
+                                                    <h5 class="mb-1 fs-3"><?= auth()->user()->identities[0]->name ?></h5>
+                                                    <span class="mb-1 d-block text-dark"><?= ucfirst(auth()->user()->getGroups()[0]) ?></span>
                                                     <p class="mb-0 d-flex text-dark align-items-center gap-2">
-                                                        <i class="ti ti-mail fs-4"></i> info@modernize.com
+                                                        <i class="ti ti-mail fs-4"></i> <?= auth()->user()->identities[0]->secret ?>
                                                     </p>
                                                 </div>
                                             </div>
                                             <div class="message-body">
                                                 <a href="./page-user-profile.html" class="py-8 px-7 mt-8 d-flex align-items-center">
-                                                    <span class="d-flex align-items-center justify-content-center bg-light rounded-1 p-6">
-                                                        <img src="<?= base_url('assets/images/svgs/icon-account.svg') ?>" alt="" width="24" height="24">
-                                                    </span>
-                                                    <div class="w-75 d-inline-block v-middle ps-3">
-                                                        <h6 class="mb-1 bg-hover-primary fw-semibold"> My Profile </h6>
-                                                        <span class="d-block text-dark">Account Settings</span>
+                                                    <div class="w-100 d-inline-block v-middle">
+                                                        <h6 class="mb-1 bg-hover-primary fw-semibold"> Edit Profil </h6>
                                                     </div>
                                                 </a>
                                             </div>
@@ -264,6 +222,8 @@
         <script src="<?= base_url('assets/js/app-style-switcher.js') ?>"></script>
         <script src="<?= base_url('assets/js/sidebarmenu.js') ?>"></script>
         <script src="<?= base_url('assets/js/custom.js') ?>"></script>
+
+        <script src="<?= base_url('assets/libs/apexcharts/dist/apexcharts.min.js') ?>"></script>
 
         <script src="<?= base_url('assets/libs/datatables.net/js/jquery.dataTables.min.js') ?>"></script>
         <script src="<?= base_url('assets/js/datatable/datatable-basic.init.js') ?>"></script>
